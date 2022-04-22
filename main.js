c7e266dd0657cabe6f0e1a6df849a54ddc2f68f9
Zentality <jmcp@live.com.au>
2022-04-22 09:49:14 +0800

    Add ability to play several rounds and store total
    Moved game round to a function so it can be called repeatedly
    Added loop to play game 5 times, and store total Wins/Losses

--- main.js
+++ main.js
@@ -1,38 +1,56 @@
-//Gets player input, makes it lowercase and trims to match output of computerPlay()
-let userSelection = prompt("Rock, Paper or Scissors?");
-userSelection = userSelection.toLowerCase().trim();
+//Loop allows 5 games and stores wins/losses
+let wins = 0;
+let losses = 0;
+for (let i = 0; i < 5; i++){
+  let roundResult = playRound();
+  //Adds total result to variables outside loop
+  if (roundResult.includes("Win")){
+    wins++;
+  } else if (roundResult.includes("Lose")){
+    losses++;
+  }
+}
 
-//Gets a result from computer play to feed into the game as a string
-let computerSelection = computerPlay();
 
-//Uses the user and computer input and returns a string stating the result (Player win, PC Win, Tie)
-let result = play(userSelection, computerSelection);
 
-console.log("User chose: " + userSelection + " and the Computer chose: " + computerSelection + " ," + result);
+//Plays a round and returns Win, Lose or Draw
+function playRound(){
+  //Gets player input, makes it lowercase and trims to match output of computerPlay()
+  let userSelection = prompt("Rock, Paper or Scissors?");
+  userSelection = userSelection.toLowerCase().trim();
 
+  //Gets a result from computer play to feed into the game as a string
+  let computerSelection = computerPlay();
+
+  //Uses the user and computer input and returns a string stating the result (Player win, PC Win, Tie)
+  let result = play(userSelection, computerSelection);
+  console.log("User chose: " + userSelection + " and the Computer chose: " + computerSelection + " ," + result);
+
+  return result;
+}
 
 //Calculates if the player or computer won
 function play(userIn, computerIn){
   if (computerIn == "scissors"){
     if (userIn == "scissors"){
-      return "draw";
+      return "Draw";
     } else if (userIn == "rock"){
-      return "player wins";
+      return "You Win!";
     }
   } else if (computerIn == "paper"){
     if (userIn == "paper"){
-      return "draw";
+      return "Draw";
     } else if (userIn == "scissors"){
-      return "player wins";
+      return "You Win!";
     }
   } else {
     if (userIn == "rock"){
-      return "draw";
+      return "Draw";
     } else if (userIn == "paper"){
-      return "player wins";
+      return "You Win!";
     }
   }
-  return "computer wins"; //Computer wins if no draws or player wins were found and returned first
+  return "You Lose!"; //Computer wins if no draws or player wins were found and returned first
 }
 
 //Selects a random decision for the computer
